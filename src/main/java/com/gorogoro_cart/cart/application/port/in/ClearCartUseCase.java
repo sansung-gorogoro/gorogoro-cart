9732084972e22fd55c9270b7577dab3dab07327f
dd-jiyun <dlwldbs8015@gmail.com>
2025-12-09 00:17:06 +0900

    refactor: 파라미터 일관성

--- src/main/java/com/gorogoro_cart/cart/application/port/in/ClearCartUseCase.java
+++ src/main/java/com/gorogoro_cart/cart/application/port/in/ClearCartUseCase.java
@@ -3,5 +3,5 @@ package com.gorogoro_cart.cart.application.port.in;
 import com.gorogoro_cart.cart.application.port.in.command.ClearCartCommand;
 
 public interface ClearCartUseCase {
-    void clearCart(ClearCartCommand command);
+    void clearCart(ClearCartCommand clearCartCommand);
 }
